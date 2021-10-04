798c0b6ae4b51bc8849c7ee609473c22a3475a89
Gabriel <ghcosta87@gmail.com>
2021-10-04 19:30:56 -0300

    file renamed to match project, constants updated

---
+++ definitions.h
@@ -0,0 +1,31 @@
+#define RX 2
+#define TX 3
+
+#define CONNECTIVITY 13//5
+
+//#define INTERRUPTPIN 2
+
+#define RIGHTMOTOR_F 6
+#define RIGHTMOTOR_B 9
+#define LEFTMOTOR_F 10
+#define LEFTMOTOR_B 11
+
+#define VOLTAGEMONITOR A0 //A0
+#define VOLTAGETIMERCHECK 1000
+
+#define FORWARD 'F'
+#define BACKWARD 'B'
+#define FRONTNEUTRAL 'n'
+#define LEFTSIDE 'L'
+#define RIGHTSIDE 'R'
+#define SIDENEUTRAL 'C'
+#define STOP 'S'
+#define STOPRELEASED 's'
+
+#define BATT0 "v0"
+#define BATT25 "v1"
+#define BATT50 "v2"
+#define BATT75 "v3"
+#define BATT100 "v4"
+
+#define DEBUG true
