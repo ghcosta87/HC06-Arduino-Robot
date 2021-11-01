a10464c97d7b0065a6519ca1e67870b981c4912e
Gabriel <ghcosta87@gmail.com>
2021-11-01 16:49:34 -0300

    join all setup functions here, added frequency increase for pwm outputs

--- SETUP.ino
+++ SETUP.ino
@@ -1,11 +1,15 @@
 void portsSetup() {
+  TCCR2B = TCCR2B & B11111000 | B00000001; // D3 e D11
+  TCCR1B = TCCR1B & B11111000 | B00000001; // D9 e D10
+  //  TCCR0B = TCCR0B & B11111000 | B00000001; // D5 e D6
+
   pinMode(RIGHTMOTOR_F, OUTPUT);
   pinMode(RIGHTMOTOR_B, OUTPUT);
   pinMode(LEFTMOTOR_F, OUTPUT);
   pinMode(LEFTMOTOR_B, OUTPUT);
 }
 
-void comSetup() {  
+void comSetup() {
   btSerial.begin(9600);
   Serial.begin(9600);
 }
